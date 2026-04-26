52c14c02b9d00b14d059909fc70527cb4cf443cc
santiago <tpedraza17@gmail.com>
2026-04-26 17:40:22 -0300

    punto 1 completado

---
+++ punto1.c
@@ -0,0 +1,99 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+typedef struct Tarea{
+int TareaID;//Numerico autoincremental comenzando en 1000
+char *Descripcion; //
+int Duracion; // entre 10-100
+} Tarea;
+
+typedef struct Nodo{
+Tarea T;
+struct Nodo *Siguiente;
+} Nodo;
+
+typedef struct{
+    struct Nodo* L;
+    int cantidad;
+}Lista;
+
+Lista crearLista(){
+    Lista nuevaL;             
+    nuevaL.L = NULL;           
+    nuevaL.cantidad = 0;       
+    return nuevaL;            
+}
+
+Nodo* crearNodo(int id, char* desc, int dur){
+    Nodo* nuevo = (Nodo*)malloc(sizeof(Nodo));
+    nuevo->T.TareaID = id;
+    nuevo->T.Duracion = dur;
+    nuevo->T.Descripcion = (char*)malloc(strlen(desc)+1);
+    strcpy(nuevo->T.Descripcion,desc);
+    nuevo->Siguiente = NULL;
+    return nuevo;
+}
+
+void insertar(Lista *h, Nodo *nuevo){
+    nuevo->Siguiente = h->L;
+    h->L = nuevo;
+    h->cantidad++;
+}
+
+void cargarTareas(Lista *pendientes) {
+    int id = 1000; 
+    int continuar=1;
+    char buffer[100];
+    int duracion;
+
+     do{
+        printf("\nNueva Tarea ID: %d\n", id);
+        printf("Descripcion: ");
+        fflush(stdin); 
+        gets(buffer); 
+        
+        printf("Duracion (10-100): ");
+        scanf("%d", &duracion);
+
+        Nodo* nuevo = crearNodo(id, buffer, duracion);
+        insertar(pendientes, nuevo);
+
+        id++;
+        printf("Desea cargar otra tarea? (1: Si / 0: No): ");
+        scanf("%d", &continuar);
+    }while(continuar==1);
+}
+
+void mostrar(Lista H){
+    Nodo* aux = H.L;                   
+    while(aux != NULL){         
+        printf("ID:%d\n", aux->T.TareaID);
+        printf("Descripcion:%s\n", aux->T.Descripcion);
+        printf("Duracion:%d\n", aux->T.Duracion);
+        aux = aux->Siguiente;   
+    }
+}
+
+void liberar(Lista *H){
+    Nodo *aux = H->L;
+    Nodo *proximo;
+
+    while(aux != NULL){
+        proximo = aux->Siguiente;
+        free(aux->T.Descripcion);
+        free(aux);
+        aux = proximo;
+    }
+}
+
+int main(){
+Lista pendiente;
+Lista realizada;
+pendiente=crearLista();
+realizada= crearLista();
+cargarTareas(&pendiente);
+mostrar(pendiente);
+liberar(&pendiente);
+    return 0;
+}
